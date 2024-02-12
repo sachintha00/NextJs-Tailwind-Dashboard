e41151bb8da546e89a0417e6ae7614d1141a2476
sachintha <sachintha0603@gmail.com>
2024-02-12 14:10:36 +0530

    #8 Fix: dashboard profile and content create and modify side bar

--- src/app/_components/sidebar/sidebar.jsx
+++ src/app/_components/sidebar/sidebar.jsx
@@ -4,61 +4,26 @@ import DashboardMenuItem from "../dashboard_menu_item/dashboard_menu_item";
 const menuItemObjects = [
   {
     menu: "Dashboard 1",
+    // link: "/dashboard",
     subMenu: [
+      {
+        // link: "/test",
+        subMenuName: "Test 1",
+        submenuItems: {
+          nestedSubMenu: [
+            { link: "/dashboard", name: "Nested Test 1" },
+            { link: "/test", name: "Nested Test 1" },
+          ],
+        },
+      },
       {
         subMenuName: "Test 1",
         submenuItems: {
-          nestedSubMenu: ["nextedsubmenu1", "nextedsubmenu2", "nextedsubmenu3"],
+          nestedSubMenu: [
+            { link: "/dashboard", name: "Nested Test 1" },
+            { link: "/test", name: "Nested Test 1" },
+          ],
         },
-      },
-      {
-        subMenuName: "Test 2",
-        submenuItems: {
-          nestedSubMenu: ["nextedsubmenu1", "nextedsubmenu2", "nextedsubmenu3"],
-        },
-      },
-      {
-        subMenuName: "Test 3",
-      },
-    ],
-  },
-  {
-    menu: "Dashboard 2",
-    subMenu: [
-      {
-        subMenuName: "Test 1",
-        submenuItems: {
-          nestedSubMenu: ["nextedsubmenu1", "nextedsubmenu2", "nextedsubmenu3"],
-        },
-      },
-      {
-        subMenuName: "Test 2",
-        submenuItems: {
-          nestedSubMenu: ["nextedsubmenu1", "nextedsubmenu2", "nextedsubmenu3"],
-        },
-      },
-      {
-        subMenuName: "Test 3",
-      },
-    ],
-  },
-  {
-    menu: "Dashboard 2",
-    subMenu: [
-      {
-        subMenuName: "Test 1",
-        submenuItems: {
-          nestedSubMenu: ["nextedsubmenu1", "nextedsubmenu2", "nextedsubmenu3"],
-        },
-      },
-      {
-        subMenuName: "Test 2",
-        submenuItems: {
-          nestedSubMenu: ["nextedsubmenu1", "nextedsubmenu2", "nextedsubmenu3"],
-        },
-      },
-      {
-        subMenuName: "Test 3",
       },
     ],
   },
